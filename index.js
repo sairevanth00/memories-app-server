a269268085d2c7cc73713bc7d65b6b8044d89937
sairevanth00 <86770889+sairevanth00@users.noreply.github.com>
2022-06-06 12:13:56 +0530

    added server files

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
 import postRoutes from "./routes/posts.js";
-import dotenv from 'dotenv';
+import userRoutes from "./routes/users.js";
+
+import dotenv from "dotenv";
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-app.get('/', (req, res) =>{
-  res.send("Hello to Memories API")
-})
+app.use("/user", userRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
+});
 // const CONNECTION_URL =
 //   "mongodb+srv://revanthsai:revanthsai123@cluster0.rljvh55.mongodb.net/?retryWrites=true&w=majority";
 
